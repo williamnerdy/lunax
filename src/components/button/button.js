7cbc071fe934af84b35c0064f1cc0557acdb6bb9
William Cunha <williamoliveira2@hotmail.com>
2018-10-17 17:03:59 -0300

    Add more styles

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,75 +1,165 @@
+// @flow
 import styled, { css } from 'styled-components';
 
-const Button = styled.button`
-  padding: 0.25em 1em;
-  margin: 0 1em;
+const Btn = styled.button`
+  display: inline-block;
+  padding: 0.8em 1.4em;
+  margin: 0.5rem 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
   cursor: pointer;
-  border-radius: 3px;
+  border-radius: 4px;
+
+  ${props =>
+    props.rounded &&
+    css`
+      border-radius: 1000px;
+    `};
+
+  ${props =>
+    props.full &&
+    css`
+      display: block;
+      width: 100%;
+    `};
+
+  ${props =>
+    props.disabled &&
+    css`
+      cursor: default;
+      opacity: 0.4;
+    `};
+`;
+
+const DefaultButton = styled(Btn)`
+  border: none;
+  color: white;
+  box-shadow: none;
+  transition: box-shadow 400ms cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  :hover {
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.25), 0 2px 2px rgba(0, 0, 0, 0.22);
+  }
+`;
+
+const TextButton = styled(Btn)`
+  border: none;
+  background: none;
 
   ${props =>
     props.primary &&
     css`
-      color: blue;
-      background: transparent;
-      border: 2px solid blue;
-
-      :hover {
-        color: white;
-        background: blue;
-      }
+      color: #18e;
     `};
 
   ${props =>
     props.secondary &&
     css`
-      color: palevioletred;
-      background: transparent;
-      border: 2px solid palevioletred;
-
-      :hover {
-        color: white;
-        background: palevioletred;
-      }
+      color: #e37;
     `};
 
   ${props =>
     props.success &&
     css`
-      color: green;
-      background: transparent;
-      border: 2px solid green;
-
-      :hover {
-        color: white;
-        background: green;
-      }
+      color: #2b2;
     `};
 
   ${props =>
     props.warning &&
     css`
-      color: yellow;
-      background: transparent;
-      border: 2px solid yellow;
-
-      :hover {
-        color: white;
-        background: yellow;
-      }
+      color: #ec5;
     `};
 
   ${props =>
-    props.error &&
+    props.danger &&
     css`
-      color: red;
-      background: transparent;
-      border: 2px solid red;
+      color: #d44;
+    `};
 
-      :hover {
-        color: white;
-        background: red;
-      }
+  ${props =>
+    props.dark &&
+    css`
+      color: #d44;
+    `};
+
+  ${props =>
+    props.dark &&
+    css`
+      color: #394050;
+    `};
+
+  ${props =>
+    props.light &&
+    css`
+      color: #f3f3f3;
     `};
 `;
+
+const OutlineButton = styled(TextButton)`
+  border-width: 1px;
+  border-style: solid;
+
+  ${props =>
+    props.primary &&
+    css`
+      border-color: #18e;
+    `};
+
+  ${props =>
+    props.secondary &&
+    css`
+      border-color: #e37;
+    `};
+
+  ${props =>
+    props.success &&
+    css`
+      border-color: #2b2;
+    `};
+
+  ${props =>
+    props.warning &&
+    css`
+      border-color: #ec5;
+    `};
+
+  ${props =>
+    props.danger &&
+    css`
+      border-color: #d44;
+    `};
+
+  ${props =>
+    props.dark &&
+    css`
+      border-color: #d44;
+    `};
+
+  ${props =>
+    props.dark &&
+    css`
+      border-color: #394050;
+    `};
+
+  ${props =>
+    props.light &&
+    css`
+      border-color: #f3f3f3;
+    `};
+`;
+
+type Props = {
+  type?: 'default' | 'text' | 'outline'
+};
+
+function Button(props: Props) {
+  const btns = {
+    default: DefaultButton,
+    text: TextButton,
+    outline: OutlineButton
+  };
+
+  return btns[props.type || 'default'];
+}
 
 export default Button;
--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -4,10 +4,139 @@ import { storiesOf } from '@storybook/react';
 import Button from './button';
 
 storiesOf('Button', module)
-  .add('with primary color', () => <Button primary>Primary Button</Button>)
-  .add('with secondary color', () => (
-    <Button secondary>Secondary Button</Button>
+  .add('Default', () => (
+    <React.Fragment>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button success>Success</Button>
+      <Button warning>Warning</Button>
+      <Button danger>Danger</Button>
+      <Button dark>Dark</Button>
+      <Button light>Light</Button>
+    </React.Fragment>
   ))
-  .add('with success color', () => <Button success>Success Button</Button>)
-  .add('with warning color', () => <Button warning>Warning Button</Button>)
-  .add('with error color', () => <Button error>Error Button</Button>);
+  .add('Rounded', () => (
+    <React.Fragment>
+      <Button primary rounded>
+        Primary
+      </Button>
+      <Button secondary rounded>
+        Secondary
+      </Button>
+      <Button success rounded>
+        Success
+      </Button>
+      <Button warning rounded>
+        Warning
+      </Button>
+      <Button danger rounded>
+        Danger
+      </Button>
+      <Button dark rounded>
+        Dark
+      </Button>
+      <Button light rounded>
+        Light
+      </Button>
+    </React.Fragment>
+  ))
+  .add('Outline', () => (
+    <React.Fragment>
+      <Button primary outline>
+        Primary
+      </Button>
+      <Button secondary outline>
+        Secondary
+      </Button>
+      <Button success outline>
+        Success
+      </Button>
+      <Button warning outline>
+        Warning
+      </Button>
+      <Button danger outline>
+        Danger
+      </Button>
+      <Button dark outline>
+        Dark
+      </Button>
+      <Button light outline>
+        Light
+      </Button>
+    </React.Fragment>
+  ))
+  .add('Text', () => (
+    <React.Fragment>
+      <Button primary text>
+        Primary
+      </Button>
+      <Button secondary text>
+        Secondary
+      </Button>
+      <Button success text>
+        Success
+      </Button>
+      <Button warning text>
+        Warning
+      </Button>
+      <Button danger text>
+        Danger
+      </Button>
+      <Button dark text>
+        Dark
+      </Button>
+      <Button light text>
+        Light
+      </Button>
+    </React.Fragment>
+  ))
+  .add('Full width', () => (
+    <React.Fragment>
+      <Button primary full>
+        Primary
+      </Button>
+      <Button secondary full>
+        Secondary
+      </Button>
+      <Button success full>
+        Success
+      </Button>
+      <Button warning full>
+        Warning
+      </Button>
+      <Button danger full>
+        Danger
+      </Button>
+      <Button dark full>
+        Dark
+      </Button>
+      <Button light full>
+        Light
+      </Button>
+    </React.Fragment>
+  ))
+  .add('Disabled', () => (
+    <React.Fragment>
+      <Button primary disabled>
+        Primary
+      </Button>
+      <Button secondary disabled>
+        Secondary
+      </Button>
+      <Button success disabled>
+        Success
+      </Button>
+      <Button warning disabled>
+        Warning
+      </Button>
+      <Button danger disabled>
+        Danger
+      </Button>
+      <Button dark disabled>
+        Dark
+      </Button>
+      <Button light disabled>
+        Light
+      </Button>
+    </React.Fragment>
+  ));
